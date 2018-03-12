583c8fe54f5d34949ad14906fa93beb72abd7974
aspiringguru <bmatthewtaylor@gmail.com>
2018-03-12 13:31:39 +1000

    86 completed and debugged

--- section6/resources/user.py
+++ section6/resources/user.py
@@ -3,10 +3,8 @@ from flask_restful import Resource, reqparse
 from models.user import UserModel
 
 class UserRegister(Resource):
-
     parser = reqparse.RequestParser()
     #https://flask-restful.readthedocs.io/en/0.3.6/api.html#module-reqparse
-
     parser.add_argument(    'username',
                             type=str,
                             required=True,
@@ -24,16 +22,10 @@ class UserRegister(Resource):
 
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists."}
+        #user = UserModel(data['username'], data['password'])
+        user = UserModel(**data)
+        #type(user) = models/user.UserModel
+        user.save_to_db()
 
-        #https://flask-restful.readthedocs.io/en/0.3.6/api.html#module-reqparse
-        #parse_args(req=None, strict=False)
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        #NULL since table users nominates field 'id'  as INTEGER PRIMARY KEY
-        #ie: users.id is auto-incrementing
-        cursor.execute(query, (data['username'], data['password']))
-        connection.commit()
-        connection.close()
         return{"message": "User created successfully."}, 201
         #https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201
