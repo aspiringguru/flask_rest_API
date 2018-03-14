ce3ec4467f8d7d626628d5125ff53c973d44217f
aspiringguru <bmatthewtaylor@gmail.com>
2018-03-14 18:52:30 +1000

    L86 completed and tested

--- section6/resources/user.py
+++ section6/resources/user.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+import sys, traceback
 
 
 
@@ -18,18 +19,18 @@ class UserRegister(Resource):
     )
 
     def post(self):
-        data = UserRegister.parser.parse_args()
+        print('resources.user.UserRegister.post() called.', file=sys.stderr)
+        try:
+            data = UserRegister.parser.parse_args()
+            print("data['username']= {}, data['password']={}".format(data['username'], data['password']), file=sys.stderr)
 
-        if UserModel.find_by_username(data['username']):
-            return {"message": "User with that username already exists."}, 400
+            if UserModel.find_by_username(data['username']):
+                return {"message": "User with that username already exists."}, 400
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
-
-        return {"message": "User created successfully."}, 201
+            #user = UserModel(data['username'], data['password'])
+            user = UserModel(**data)
+            user.save_to_db()
+            return {"message": "User created successfully."}, 201
+        except:
+            traceback.print_exc(file=sys.stderr)
+            return {"message": "An error occurred in resources.user.UserRegister.post()."}, 500
